31a3f6e7a4072e36447fd083456bfe6c87c44b0f
wiaan <whoppingwaki@gmail.com>
2022-12-17 18:14:38 +0200

    updated the home page and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import image from '../assets/programming.jpg'
+
 function Home() {
   return (
-    <div className="Home">
-        <h1>Sweet Home Alabama</h1>
+    <div className="home">
+        <div className="image-banner">
+            <img src={image} alt="Programming Code Background" />
+        </div>
+        <h1>welcome to my website, enjoy your stay :))</h1>
     </div>
   )
 }
